738c1b29661b251f3a16c2c004c48c1e86c3601e
shanu agrawal <agrawalshanu798@gmail.com>
2021-12-02 17:08:35 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const dateOfBirth = document.querySelector("#date-of-birth")
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button")
-const outputDiv = document.querySelector("#output-div")
+const outputDiv =   document.querySelector("#output-div")
 
 checkButton.addEventListener('Click' , checkLucky) 
 
 
 function compareValue(sum ,luckyNumber){
+  
   if(sum%luckyNumber === 0){
     outputDiv.innerText= "your birthday is lucky"
   }else{
@@ -15,16 +16,20 @@ function compareValue(sum ,luckyNumber){
 }
 
 function checkLucky(){
+
   const dob = dateOfBirth.value;
   const sum = calculate(dob);
   compareValue(sum, luckyNumber.value)
+
 }
 
 function calculate(dob){
+
   dob = dob.replaceAll("_", "");
   let sum = 0;
   for(let index = 0; index < dob.length; index++){
     sum = sum + Number(dob.charAt(index));
   }
   return sum;
+
 }
