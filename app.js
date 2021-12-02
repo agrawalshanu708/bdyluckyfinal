70a4d8028704c529a159e5494039925b12e3f914
shanu agrawal <agrawalshanu798@gmail.com>
2021-12-02 11:53:17 +0530

    updated

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-var dateOfBirth = document.querySelector("#date-of-birth")
-var luckyNumber = document.querySelector("#lucky-number")
-var checkButton = document.querySelector("#check-button")
+const dateOfBirth = document.querySelector("#date-of-birth")
+const luckyNumber = document.querySelector("#lucky-number")
+const checkButton = document.querySelector("#check-button")
+const outputDiv   = document.querySelector("#output-div")
 
-checkButton.addEventListener("click", function clickValidate() {
+checkButton.addEventListener("click", function calculateDateOfBirth(dateOfBirth.value) {
 
-    var dob = dateOfBirth.value;
-    var sum = calculateDateOfBirth(dob);
-    console.log(sum);
+    //let dob = dateOfBirth.value;
+    //calculateDateOfBirth(dateOfBirth.value);
+     if(sum % luckyNumber === 0){
+      outputDiv.innerText = "you are lucky"
+     }else{
+      outputDiv.innerText = "sorry you are not"
+     }
 
 })
 
-
-   
-
-
-
-
-function calculateDateOfBirth (dob){
-  dob = dob.replaceAll("-", "");
-  var sum = 0;
+function calculateDateOfBirth (dateOfBirth.value){
+  let dob = dateOfBirth.value.replaceAll("-", "");
+  let sum = 0;
   for(let index=0; index<dob.length; index++){
       sum = sum + Number(dob.charAt(index));
-   
 }
-
   return sum;
 }
